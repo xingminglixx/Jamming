fc8ebc57e4e5a874ab332dac7ca22e49a843132b
xingminglixx <lix171@mytru.ca>
2021-06-17 16:58:33 -0700

    good

---
+++ src/Components/App/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+
+render(){
+retrun
+(
+<div>
+  <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <div className="App">
+     <-- Add a SearchBar component -->
+    <div className="App-playlist">
+      <!-- Add a SearchResults component -->
+  <!-- Add a Playlist component -->
+    </div>
+  </div>
+</div>
+
+
+
+)
+}
+
+
+
+
+
+}
+
+export default App;
