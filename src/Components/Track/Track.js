e04eaad1b9b347ce842d20bd24e0ed1ba3ab9375
xingminglixx <lix171@mytru.ca>
2021-06-17 17:57:51 -0700

    good

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,1 +1,22 @@
 import React from 'react';
+import './Track.css';
+
+
+export class Track extends React.Component {
+
+render() {
+    return (
+        <div class="Track">
+  <div className="Track-information">
+    <h3><!-- track name will go here --></h3>
+    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+  </div>
+  <button className="Track-action"><!-- + or - will go here --></button>
+</div>
+
+
+    )
+}
+
+
+}
