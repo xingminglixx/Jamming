11c152e9a4815b5204932b3990f61acce75d111b
xingminglixx <lix171@mytru.ca>
2021-06-18 15:17:27 -0700

    good

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import './App.css';
-
+import {SearchBar} from '../SearchBar/SearchBar'
+import {SearchResults} from '../SearchResults/SearchResults'
+import {Playlist} from '../Playlist/Playlist'
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{
+          name: null,
+          artist: null,
+          album: null,
+          id: null
+        },
+        {
+          name: null,
+          artist: null,
+          album: null,
+          id: null
+        },
+        {
+          name: null,
+          artist: null,
+          album: null,
+          id: null
+        }
+      ]
+    }
+  }
+
 
 render(){
 return(
 <div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
-  <!-- Add a SearchBar component -->
+  <SearchBar />
     <div className="App-playlist">
-    <!-- Add a SearchResults component -->
-    <!-- Add a Playlist component -->
+   <SearchResults searchResults={this.state.searchResults} />
+    <Playlist/>
     </div>
     </div>
   </div>
-</div>
+  
 
 
 
--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Tracklist.css'
-
+import {Track} from '../Track/Track'
 export class Tracklist extends React.Component {
 
 render() {
     return (
         <div className="TrackList">
-       <!-- You will add a map method that renders a set of Track components  -->
+            {this.props.tracks.map(track => {
+                return 
+
+
+
+
+            })}
     </div>
 
 
